d33f3902053b7efb4a502f8dde4b18ffc68e393f
Fernando <fernandxmacedo@gmail.com>
2026-09-10 00:01:23 -0300

    commit inicial

---
+++ src/test/java/com/teste/agendador/AgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
