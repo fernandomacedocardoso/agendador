05adf158af2a0c72d86221a1e24eac67908f043a
Fernando <fernandxmacedo@gmail.com>
2026-09-10 22:07:53 -0300

    configuraçoes de segurança

---
+++ src/main/java/com/teste/agendador/business/dto/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package com.teste.agendador.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
